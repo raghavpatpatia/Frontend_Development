6a5713d3309d8bb9ddd53f8cf39fbe4c7adc3da6
Raghav <63911252+raghavpatpatia@users.noreply.github.com>
2020-11-26 10:34:35 +0530

    Frontend Created using HTML5 and CSS

---
+++ Dream Travels/js/script.js
@@ -0,0 +1,11 @@
+function popup() {
+    alert("Thank you for making a reservation, we'll get back to you soon.");
+}
+
+function abc() {
+    alert("Thank you for subscribing to our Newsletter. Your subscription is successful.");
+}
+
+function def() {
+    alert("Thank you for your message. You will receive a response shortly.");
+}
